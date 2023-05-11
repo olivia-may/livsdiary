1a3669c431551673da0c96d74f404ac995ab85cc
Olivia May <olivia.may@tuta.io>
2023-05-10 20:31:06 -0500

    Update clean.py

--- clean.py
+++ clean.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python
 
 import shutil
+import glob
+import os
 
 print('rm -rf muon/')
 shutil.rmtree('muon/', ignore_errors=True)
 print('rm -rf build/')
 shutil.rmtree('build/', ignore_errors=True)
+print('rm -f *.html')
+remove_list = glob.glob('*.html')
+for i in range(0, len(remove_list)):
+    os.remove(remove_list[i])
