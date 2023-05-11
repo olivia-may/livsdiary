bdb3d4e45e06feeac3a05e27f6cb0e61b0480599
Olivia May <olivia.may@tuta.io>
2023-05-10 19:06:41 -0500

    Update muon install script

--- install-muon.py
+++ install-muon.py
@@ -2,10 +2,17 @@
 
 import os
 
+print('git clone https://git.sr.ht/~lattis/muon/')
 os.system('git clone https://git.sr.ht/~lattis/muon/')
-os.chdir('muon')
+print('cd muon/')
+os.chdir('muon/')
+print('meson setup build')
 os.system('meson setup build')
+print('cd build')
 os.chdir('build')
+print('ninja')
 os.system('ninja')
+print('meson test')
 os.system('meson test')
+print('meson install')
 os.system('meson install')
